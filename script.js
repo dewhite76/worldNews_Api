5342c9c2e609c2109edb980c562533b3ca4f94aa
dewhite <osenisaheed76@gmail.com>
2023-12-06 18:10:01 +0100

    mynew world news

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let row = document.getElementById('row')
-const apiKey = "336507b0d4764d92834b29fe8cb71bc8";
+const apiKey = "f5c0ade1ff294bdc9b4f7304dd84f60b";
 function fetchNews(){
     fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`)
+      // `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`)
+      `https://api.worldnewsapi.com/search-news?api-key=${apiKey}&text=tesla`
+    )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.articles);
-        data.articles.map((value, i) => {
+        console.log(data.news);
+        data.news.map((value, i) => {
           row.innerHTML += `
                 <div class='flex-cont'>
-                   <img src="${value.urlToImage}">
+                   <img src="${value.image}">
                    <strong>Author:</strong> <cite>${value.author}</cite>
-                   <p>${value.description}</p>
-                   <p>${value.conten}</p>
+                   <p>${value.source_country}</p>
+                   <p>${value.text.slice(0, 10)}</p>
                     <p>${value.title}</p>
                     <div>
                     <button class="btn-more">
